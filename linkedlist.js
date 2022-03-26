dd37eebf466a118f58d3009ca9c85b9e252f5766
Movind26 <85275381+Movind26@users.noreply.github.com>
2022-03-26 03:51:37 +0300

    Update LinkedList
    Created class Node and their constructor(data).
    Created print() and find(elem) methods.
    
    Fixed append(), prepend(), static fromIterable(iterable) methods.

--- linkedlist.js
+++ linkedlist.js
@@ -1,3 +1,10 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor() {
     this.head = null;
@@ -5,44 +12,80 @@ class LinkedList {
     this.length = 0;
   }
 
-  append(value) {
+  append(elem) {
     this.length++;
-    let newNode = createNode(value);
+    let temp = new Node(elem);
 
     if (this.link) {
-      this.link.next = newNode;
-      this.link = newNode;
-      return newNode;
+      this.link.next = temp;
+      this.link = temp;
+      return temp;
     }
   
-    this.head = this.link = newNode;
-    return newNode;
+    this.head = this.link = temp;
+    return temp;
   }
 
-  prepend(value) {
+  prepend(elem) {
     this.length++;
-    let newNode = createNode(value);
+    let temp = new Node(elem);
 
     if (this.head) {
-      newNode.next = this.head;
-      this.head = newNode;
-      return newNode;
+      temp.next = this.head;
+      this.head = temp;
+      return temp;
     }
 
-    this.head = this.link = newNode;
-    return newNode;
+    this.head = this.link = temp;
+    return temp;
+  }
+
+  find(elem) {
+    let current = this.head;
+
+    while (current !== null) {
+      if (current.data == elem) {
+        return current.data;
+      }
+
+      current = current.next;
+    }
+
+    return null;
   }
 
   toArray() {
     let result = [];
-    let current = this.top;
+    let current = this.head;
 
     while(current !== null) {
       result.push(current.data);
-      current = current.link;
+      current = current.next;
     }
     
     return result;
   }
 
+  print() {
+    let current = this.head;
+
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+
+  static fromIterable(iterable) {
+    if (typeof iterable[Symbol.iterator] !== 'function') {
+      throw new Error('The entity is not iterable');
+    }
+
+    const result = new LinkedList();
+
+    for (let i = 0; i < iterable.length; i++) {
+      result.prepend(iterable[i]);
+    }
+
+    return result;
+  }
 }
